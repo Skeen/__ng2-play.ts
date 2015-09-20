71b74e38270010ef1e54c6ddc0b90f68b6e25e17
Emil 'Skeen' Madsen <sovende@gmail.com>
2015-09-20 13:13:36 +0200

    Initial release of the Typescript Angular2 HTTP example.

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,17 @@ var gulp = require('gulp');
 var PATHS = {
     src: {
         js: 'src/**/*.ts',
-        html: 'src/**/*.html'
+        html: 'src/**/*.html',
+        res: 'res/**/*'
     },
     lib: [
         'node_modules/angular2/node_modules/traceur/bin/traceur-runtime.js',
-        'node_modules/angular2/bundles/angular2.min.js',
+        'node_modules/angular2/bundles/angular2.dev.js',
+        'node_modules/angular2/bundles/http.dev.js',
         'node_modules/systemjs/dist/system-csp-production.js'
     ],
-    typings: 'node_modules/angular2/bundles/typings/angular2/angular2.d.ts'
+    ng2Typing:'node_modules/angular2/bundles/typings/angular2/angular2.d.ts',
+    ng2HttpTyping : 'node_modules/angular2/bundles/typings/angular2/http.d.ts'
 };
 
 gulp.task('clean', function (done) {
@@ -20,7 +23,7 @@ gulp.task('clean', function (done) {
 
 gulp.task('js', function () {
     var typescript = require('gulp-typescript');
-    var tsResult = gulp.src([PATHS.src.js, PATHS.typings])
+    var tsResult = gulp.src([PATHS.src.js, PATHS.ng2Typing, PATHS.ng2HttpTyping])
         .pipe(typescript({
             noImplicitAny: true,
             module: 'system',
@@ -28,12 +31,15 @@ gulp.task('js', function () {
             emitDecoratorMetadata: true,
             experimentalDecorators: true
         }));
-
     return tsResult.js.pipe(gulp.dest('dist'));
 });
 
 gulp.task('html', function () {
     return gulp.src(PATHS.src.html).pipe(gulp.dest('dist'));
+});
+
+gulp.task('res', function () {
+    return gulp.src(PATHS.src.res).pipe(gulp.dest('dist'));
 });
 
 gulp.task('libs', function () {
@@ -50,6 +56,7 @@ gulp.task('play', ['libs', 'html', 'js'], function () {
 
     gulp.watch(PATHS.src.html, ['html']);
     gulp.watch(PATHS.src.js, ['js']);
+    gulp.watch(PATHS.src.res, ['res']);
 
     app = connect().use(serveStatic(__dirname + '/dist'));  // serve everything that is static
     http.createServer(app).listen(port, function () {
--- src/hello.ts
+++ src/hello.ts
@@ -1,19 +1,22 @@
-import {Component, View, bootstrap, CORE_DIRECTIVES} from 'angular2/angular2';
+import {Component, View, bootstrap, Injectable, NgIf} from 'angular2/angular2';
+import {Http} from 'angular2/http';
+import {HTTP_BINDINGS} from 'angular2/http';
 
 @Component({
     selector: 'hello'
 })
 @View({
-    template: `<span *ng-if="name">Hello, {{name}}!</span>`,
-    directives: [CORE_DIRECTIVES]
+    template: `<span ng-if="name">Hello, {{name}}!</span>`
 })
+@Injectable()
 export class Hello {
     name: string = 'World';
-    constructor() {
+    constructor(http:Http) {
+        http.get('./http_request.txt').toRx().subscribe(res => alert(res.text()));
         setTimeout(() => {
             this.name = 'NEW World'
         }, 2000);
     }
 }
 
-bootstrap(Hello);
+bootstrap(Hello, [HTTP_BINDINGS]);
